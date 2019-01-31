ec26188eaea9a743e595f5a751df43e34332c36d
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2019-01-31 23:34:28 +0100

    wip

--- app/src/slizaatree/SlizaaTreeComponentNode.ts
+++ app/src/slizaatree/SlizaaTreeComponentNode.ts
@@ -5,10 +5,12 @@ export class SlizaaTreeComponentNode {
     public expanded: boolean;
     public parent?: SlizaaTreeComponentNode;
     public children: SlizaaTreeComponentNode[];
+    public hasChildren: boolean;
 
     constructor(key: string, title: string, parent?: SlizaaTreeComponentNode) {
         this.key = key;
         this.title = title;
         this.parent = parent;
+        this.hasChildren = true;
     }
 }
